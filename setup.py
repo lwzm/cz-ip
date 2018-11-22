eb6fefe128473ebd081420ac72bc3816a1c43f85
q <lwzm@qq.com>
2018-11-22 11:10:38 +0800

    tune README and LICENSE

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 from setuptools import setup
 
@@ -6,9 +6,14 @@ import cz_ip
 
 author = "lwzm"
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     version=cz_ip.__version__,
     description="Find the location of ip address",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author=author,
     author_email="{}@qq.com".format(author),
     keywords="ip address chunzhen cz88".split(),
@@ -16,4 +21,19 @@ setup(
     name="cz-ip",
     packages=["cz_ip"],
     package_data={"cz_ip": ['ip.db']},
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
